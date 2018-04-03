28a8a8a0b880992bd1b5c070c7073124dc06d2a6
hghleow <gretaleow@gmail.com>
2018-04-03 15:38:49 +0800

    added logic to router.post code in user.js

--- Controllers/user.js
+++ Controllers/user.js
@@ -4,7 +4,8 @@ const User = require('../Models/user');
 
 router.post("/authenticate", (request, response) => {
     //find whether user exists
-    
+    //if user doesn't exist, save their credentials in DB first, then immediately redirect user to another page to get their full name
+    //of user exists but pw wrong, shake the form while showing the red cursor  
 });
 
 module.exports = router;
