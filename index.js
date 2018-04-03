926ece34319eb84203e60abb146ae13fe2aeed1e
hghleow <gretaleow@gmail.com>
2018-04-03 09:54:36 +0800

    created mvc, handlebars, js, css file

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+//uses custom template; contnt runs on handlebars
+app.engine(
+	"handlebars",
+	handlebars({
+		defaultLayout: "main"
+	})
+);
+
+app.set("view engine", "handlebars");
+
+app.use(express.static("Static"));
+
+//tell your app to use the module
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+// root dir
+app.get("/", (req, res) => {
+	res.render("home", {
+		title: "TooDoo",
+		name: "Greta"
+	});
+});
+
+// login route
+app.get("/login", (req, res) => {
+	res.render("login", {
+		title: "TooDoo | Login"
+	});
+});
+
+app.post("/login", (req, res) => {
+	console.log(req.body.email);
+});
+
+app.listen(3000);
+
+
+// add to-do route
+app.get("/add", (req, res) => {
+	res.render("add", {
+		title: "TooDoo"
+	});
+});
